4e15ed6edd0058ab66e7a7bc23803bd8a4200be5
togondol <obimbira60@gmail.com>
2024-08-29 13:23:06 +0300

    find Santa the lowest positive number (no leading zeroes: 1, 2, 3, ...) that produces such an MD5 hash starting with atleast six zeros

---
+++ day_4/part2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "crypto/md5"
+    "fmt"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    data := "iwrupvqb"
+    targetPrefix := "000000"
+    var hash [16]byte
+    counter := 1
+
+    for {
+        newData := data + strconv.Itoa(counter)
+        
+        // Compute the MD5 hash
+        hash = md5.Sum([]byte(newData))
+        
+        // Convert the hash to a hexadecimal string
+        hashString := fmt.Sprintf("%x", hash)
+        
+        // Check if the hash starts with the desired prefix
+        if strings.HasPrefix(hashString, targetPrefix) {
+            fmt.Printf("Found hash: %s\n", hashString)
+            fmt.Printf("With counter: %d\n", counter)
+            break
+        }
+
+        // Increment the counter for the next iteration
+        counter++
+    }
+}
